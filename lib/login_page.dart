eb188a99b0e2dcab58a25e2c2c0236f093a6fcbe
Ivan Trj <ivantrajanovski@gmail.com>
2022-05-09 21:46:21 +0200

    add project

---
+++ lib/login_page.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login UI"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 100),
+            child: Image.asset(
+              "images/login_logo.png",
+              height: 150,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(3),
+            child: CupertinoButton(
+              onPressed: () {},
+              child: Text("Login"),
+              color: CupertinoColors.activeGreen,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
